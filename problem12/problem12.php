7a20c866dfab6c9c4fe7de9d28b8347eabd61360
david seroney <dvdseroney@gmail.com>
2016-04-02 23:44:49 +0300

    add function to return array of divisors

--- problem12/problem12.php
+++ problem12/problem12.php
@@ -6,4 +6,13 @@ function getTriNumber($mynum){
 	}
 	return $retVal;
 }
+function getFactor($mynum2){
+	$myArray=array();
+	for ($i=0; $i < $mynum2; $i++) { 
+		if ($mynum2 % ($i+1) == 0) {
+			array_push($myArray, $i+1);
+		}
+	}
+	return $myArray;
+}
 ?>
