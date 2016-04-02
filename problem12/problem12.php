fd2e19f124123c6aaccfb187b8b0773a5ae72739
david seroney <dvdseroney@gmail.com>
2016-04-03 02:06:00 +0300

    finish problem 12 (square root on getfactor's forloop)

--- problem12/problem12.php
+++ problem12/problem12.php
@@ -5,14 +5,24 @@ function getTriNumber($mynum){
 		$retVal=$retVal+$i+1;
 	}
 	return $retVal;
+	echo "trinumber is: ".$retVal."<br>";
 }
-function getFactor($mynum2){
+function getFactors($mynum2){
 	$myArray=array();
-	for ($i=0; $i < $mynum2; $i++) { 
+	for ($i=0; $i < sqrt($mynum2); $i++) { 
 		if ($mynum2 % ($i+1) == 0) {
 			array_push($myArray, $i+1);
+			array_push($myArray, $mynum2 / ($i+1));
 		}
 	}
 	return $myArray;
+	echo "array returned"."<br>";
+}
+for ($i=1; $i > 0; $i++) { 
+	$myTriNo=getTriNumber($i);
+	$triFactors=getFactors($myTriNo);
+	if(sizeof($triFactors) > 500){
+		echo $myTriNo."<br>";
+	}
 }
 ?>
