9ed693cf08aa62d44f644803f9621bc28db3a921
pankpinther <dvdseroney@gmail.com>
2016-04-04 09:56:28 -0400

    attempt different approach on problem 14

--- problem14/problem14.php
+++ problem14/problem14.php
@@ -7,13 +7,22 @@ function nextNum($num){
 		return ($num * 3)+1;
 	}
 }
-for ($i=0; $i < 1000000; $i++) { 
+function seriesLengthArray($mynum){
+	$seriesArray=array();
+	array_push($seriesArray,$mynum);
+	while($mynum!=1){
+		$mynum=nextNum($mynum);
+		array_push($seriesArray,$mynum);
+	}
+	return $seriesArray;
+}
+/*for ($i=0; $i < 1000000; $i++) { 
 	$counter=0;
 	$checker=$i;
 	while($checker != 1){
 		$checker=nextNum($checker);
 		$counter++;
 	}
-}
-echo $counter;
+}*/
+//echo $counter;
 ?>
